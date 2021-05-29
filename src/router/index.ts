93999df75b5e002243b7b6c0ad75fca4d36d338b
Edward <e.arechwa@gmail.com>
2021-05-29 23:19:05 +0200

    create routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,65 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
-const routes: Array<RouteRecordRaw> = [];
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: "/categories",
+        name: "categories",
+        meta: { layout: "base" },
+        component: () => import("../views/Categories.vue"),
+    },
+    {
+        path: "/history",
+        name: "History",
+        meta: { layout: "base" },
+        component: () => import("../views/History.vue"),
+    },
+    {
+        path: "/home",
+        name: "Home",
+        meta: { layout: "base" },
+        component: () => import("../views/Home.vue"),
+    },
+    {
+        path: "/login",
+        name: "Login",
+        meta: { layout: "base" },
+        component: () => import("../views/Login.vue"),
+    },
+    {
+        path: "/planing",
+        name: "Planing",
+        meta: { layout: "base" },
+        component: () => import("../views/Planing.vue"),
+    },
+    {
+        path: "/profile",
+        name: "Profile",
+        meta: { layout: "base" },
+        component: () => import("../views/Profile.vue"),
+    },
+    {
+        path: "/record",
+        name: "Record",
+        meta: { layout: "base" },
+        component: () => import("../views/Record.vue"),
+    },
+    {
+        path: "/records",
+        name: "Records",
+        meta: { layout: "base" },
+        component: () => import("../views/Records.vue"),
+    },
+    {
+        path: "/register",
+        name: "Register",
+        meta: { layout: "base" },
+        component: () => import("../views/Register.vue"),
+    },
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
 export default router;
