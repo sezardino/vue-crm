73b21e9f8b1344496e137f3bc3c6ccbbfda46fde
Edward <e.arechwa@gmail.com>
2021-05-30 21:40:18 +0200

    add firebase error messages

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -1,3 +1,9 @@
 export default {
   logout: "You are logout",
+  "auth/email-already-in-use":
+    "The email address is already in use by another account.",
+  "auth/wrong-password":
+    "The password is invalid or the user does not have a password.",
+  "auth/user-not-found":
+    "There is no user record corresponding to this identifier. The user may have been deleted.",
 };
