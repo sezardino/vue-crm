06cb800ce8c902563f81dfc4390fa9697eb1d225
Edward <e.arechwa@gmail.com>
2021-05-30 21:40:02 +0200

    initialize firebase

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,30 @@ import store from "./store";
 import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize.min";
 import "./assets/index.css";
+import messagePlugin from "./utils/message";
 
-createApp(App).use(store).use(router).mount("#app");
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMNbsNmv--3rf8JLCoRlNnSE6NGMlHEJg",
+  authDomain: "finance-crm-fe97c.firebaseapp.com",
+  projectId: "finance-crm-fe97c",
+  storageBucket: "finance-crm-fe97c.appspot.com",
+  messagingSenderId: "184747597044",
+  appId: "1:184747597044:web:543bfffe1d81b9ed3712a1",
+};
+
+firebase.initializeApp(firebaseConfig);
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App);
+    app.use(store);
+    app.use(router);
+    app.use(messagePlugin);
+    app.mount("#app");
+  }
+});
