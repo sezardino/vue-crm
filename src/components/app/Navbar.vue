7593335838713d473e0ec3b553d73ac9be794792
Edward <e.arechwa@gmail.com>
2021-05-30 10:55:37 +0200

    add work with date

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -5,7 +5,7 @@
         <a href="#" @click="$emit('toggleSidebar')">
           <i class="material-icons black-text">dehaze</i>
         </a>
-        <span class="black-text">12.12.12</span>
+        <span class="black-text">{{ formatedDate }}</span>
       </div>
 
       <ul class="right hide-on-small-and-down">
@@ -42,10 +42,44 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import { Dropdown } from "materialize-css/dist/js/materialize.min";
+
 export default defineComponent({
   emits: ["toggleSidebar", "logout"],
+  data() {
+    return {
+      date: new Date(),
+      interval: null as null | number,
+      dropdown: null as null | any,
+    };
+  },
   mounted() {
-    new Dropdown(this.$refs.dropdown, { constrainWidth: true });
+    this.dropdown = new Dropdown(this.$refs.dropdown, { constrainWidth: true });
+
+    this.interval = setInterval(() => (this.date = new Date()), 1000);
+  },
+
+  beforeUnmount() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+
+    if (this.dropdown && this.dropdown.destroy) {
+      this.dropdown.destroy();
+    }
+  },
+
+  computed: {
+    formatedDate(): string {
+      const options = {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      };
+      return new Intl.DateTimeFormat("en-Pl", options).format(this.date);
+    },
   },
 });
 </script>
