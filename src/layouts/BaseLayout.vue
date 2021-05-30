7c490ad4bf5e2dfe3cd17be732b29e739ee673f3
Edward <e.arechwa@gmail.com>
2021-05-30 09:32:48 +0200

    change name

---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app-main-layout">
+    <navbar @toggleSidebar="sidebarVisibility = !sidebarVisibility"></navbar>
+
+    <sidebar v-model:isOpen="sidebarVisibility"></sidebar>
+
+    <main class="app-content" :class="!sidebarVisibility ? 'full' : ''">
+      <div class="app-page">
+        <router-view></router-view>
+      </div>
+    </main>
+
+    <div class="fixed-action-btn">
+      <router-link class="btn-floating btn-large blue" to="/record">
+        <i class="large material-icons">add</i>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Sidebar from "@/components/app/Sidebar.vue";
+import Navbar from "../components/app/Navbar.vue";
+
+export default defineComponent({
+  components: { Navbar, Sidebar },
+  data() {
+    return {
+      sidebarVisibility: true,
+    };
+  },
+});
+</script>
