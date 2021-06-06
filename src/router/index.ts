38919670495f6e6f5249fc5a67e9519780f59ff1
Edward <e.arechwa@gmail.com>
2021-06-06 10:39:18 +0200

    add route protection

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,46 +1,47 @@
+import firebase from "firebase/app";
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/categories",
     name: "categories",
-    meta: { layout: "base" },
+    meta: { layout: "base", auth: true },
     component: () => import("../views/Categories.vue"),
   },
   {
     path: "/history",
     name: "History",
-    meta: { layout: "base" },
+    meta: { layout: "base", auth: true },
     component: () => import("../views/History.vue"),
   },
   {
     path: "/",
     name: "Home",
-    meta: { layout: "base" },
+    meta: { layout: "base", auth: true },
     component: () => import("../views/Home.vue"),
   },
   {
     path: "/planing",
     name: "Planing",
-    meta: { layout: "base" },
+    meta: { layout: "base", auth: true },
     component: () => import("../views/Planing.vue"),
   },
   {
     path: "/profile",
     name: "Profile",
-    meta: { layout: "base" },
+    meta: { layout: "base", auth: true },
     component: () => import("../views/Profile.vue"),
   },
   {
     path: "/record",
     name: "Record",
-    meta: { layout: "base" },
+    meta: { layout: "base", auth: true },
     component: () => import("../views/Record.vue"),
   },
   {
     path: "/records",
     name: "Records",
-    meta: { layout: "base" },
+    meta: { layout: "base", auth: true },
     component: () => import("../views/Records.vue"),
   },
   {
@@ -60,6 +61,16 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser;
+  const reqAuth = to.meta.auth;
+  if (reqAuth && !currentUser) {
+    next("/login");
+  } else {
+    next();
+  }
 });
 
 export default router;
