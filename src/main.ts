0286ba64d823ee33669f86040f95c8bd57507e3a
Edward <e.arechwa@gmail.com>
2021-06-06 13:29:10 +0200

    add paginate plugin, comments

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,31 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+
+//materializeCss
 import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize.min";
+
+//css
 import "./assets/index.css";
+
+//plugins
 import messagePlugin from "./utils/message";
+
+//components
 import Loader from "./components/app/Loader.vue";
+import Paginate from "vuejs-paginate";
+
+//directives
 import formatDirective from "@/directives/format";
 import tooltipDirective from "@/directives/tooltip";
 
+//firebase
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
 
+//init firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAMNbsNmv--3rf8JLCoRlNnSE6NGMlHEJg",
   authDomain: "finance-crm-fe97c.firebaseapp.com",
@@ -23,14 +36,15 @@ const firebaseConfig = {
   messagingSenderId: "184747597044",
   appId: "1:184747597044:web:543bfffe1d81b9ed3712a1",
 };
-
 firebase.initializeApp(firebaseConfig);
 let app;
 
 firebase.auth().onAuthStateChanged(() => {
   if (!app) {
+    //init vue app
     app = createApp(App);
     app.component("Loader", Loader);
+    app.component("Paginate", Paginate);
     app.use(store);
     app.use(router);
     app.use(messagePlugin);
