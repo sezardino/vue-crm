2432794ea67e45c3738e858f1d74b4d74895357e
Edward <edward.arechwa@gogomedia.pl>
2021-06-07 11:12:25 +0200

    create chart

--- src/views/History.vue
+++ src/views/History.vue
@@ -4,10 +4,6 @@
       <h3>History</h3>
     </div>
 
-    <div class="history-chart">
-      <canvas></canvas>
-    </div>
-
     <Loader v-if="loading" />
 
     <p class="center" v-else-if="!currentItems.length">
@@ -16,6 +12,9 @@
     </p>
 
     <section v-else>
+      <div class="history-chart">
+        <Chart v-bind="{ ...chart }" v-if="chart" />
+      </div>
       <HistoryTable :records="currentItems" />
 
       <Paginate :total-pages="totalPages" :current-page="currentPage" />
@@ -26,32 +25,71 @@
 <script lang="ts">
 import HistoryTable from "@/components/HistoryTable.vue";
 import { defineComponent } from "vue";
+import Chart from "@j-t-mcc/vue3-chartjs";
 
 export default defineComponent({
-  components: { HistoryTable },
+  components: { HistoryTable, Chart },
   data() {
     return {
       loading: true,
       items: [],
+      records: [],
       itemsPerPage: 3,
+      chart: null,
     };
   },
   methods: {
     updatePage() {
       this.currentPage = this.$route.params.page;
     },
+    setup(categories) {
+      this.totalPages = Math.round(this.records.length / this.itemsPerPage);
+      this.totalItems = this.records.map((record) => ({
+        ...record,
+        typeClass: record.type === "income" ? "green" : "red",
+        typeText: record.type === "income" ? "Income" : "Outcome",
+        category: categories.find(
+          (category) => category.id === record.categoryId
+        ).name,
+      }));
+
+      this.chartSetup(categories);
+    },
+    chartSetup(categories) {
+      const chartData = categories.map((category) => {
+        const recordsData = this.records.filter(
+          (record) =>
+            record.categoryId === category.id && record.type === "outcome"
+        );
+        return {
+          name: category.name,
+          value: recordsData.reduce((acc, record) => acc + +record.amount, 0),
+          color:
+            "#" +
+            (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6),
+        };
+      });
+      console.log(chartData);
+      this.chart = {
+        type: "pie",
+        data: {
+          labels: chartData.map((chart) => chart.name),
+          datasets: [
+            {
+              backgroundColor: chartData.map((chart) => chart.color),
+              data: chartData.map((chart) => chart.value),
+            },
+          ],
+        },
+      };
+    },
   },
   async mounted() {
-    const records = await this.$store.dispatch("getRecords");
+    this.records = await this.$store.dispatch("getRecords");
     const categories = await this.$store.dispatch("getCategories");
-    this.totalPages = Math.round(records.length / this.itemsPerPage);
-    this.totalItems = records.map((record) => ({
-      ...record,
-      typeClass: record.type === "income" ? "green" : "red",
-      typeText: record.type === "income" ? "Income" : "Outcome",
-      category: categories.find((category) => (category.id = record.categoryId))
-        .name,
-    }));
+
+    this.setup(categories);
+
     this.loading = false;
   },
   computed: {
